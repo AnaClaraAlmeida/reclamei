24b08c2118a560d69049c7d50322223fd9f6a82d
AnaClaraAlmeida <anaclaraalmeida.jf@gmail.com>
2024-06-08 16:46:29 -0300

    x

--- reclameiApi/Program.cs
+++ reclameiApi/Program.cs
@@ -1,19 +1,18 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Adicionar servi�os CORS
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("CorsPolicy", builder =>
-    {
-        builder.AllowAnyOrigin()
-               .AllowAnyMethod()
-               .AllowAnyHeader();
+    options.AddPolicy("CorsPolicy", builder => {
+        builder.AllowAnyOrigin();
+        builder.AllowAnyMethod();
+        builder.AllowAnyHeader();
     });
 });
 
@@ -28,36 +27,10 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseRouting(); // Adicione esta linha
-
-// Aplicar a pol�tica CORS
 app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
-app.UseSession();
-
 app.MapControllers();
 
 app.Run();
-
-public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-{
-    if (env.IsDevelopment())
-    {
-        app.UseDeveloperExceptionPage();
-    }
-
-    app.UseHttpsRedirection();
-    app.UseStaticFiles();
-
-    app.UseRouting();
-
-    app.UseSession(); // Adiciona o middleware de sessão
-    app.UseAuthorization();
-
-    app.UseEndpoints(endpoints =>
-    {
-        endpoints.MapRazorPages();
-    });
-}
