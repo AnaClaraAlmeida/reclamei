506beaa19718a0d84f180f46a089b812a529bfdb
AnaClaraAlmeida <anaclaraalmeida.jf@gmail.com>
2024-06-08 16:01:13 -0300

    teste

--- reclameiFront/Program.cs
+++ reclameiFront/Program.cs
@@ -7,7 +7,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-
+//builder.Services.AddScoped(sp => new Api("http://localhost:5024"));
 
 await builder.Build().RunAsync();
