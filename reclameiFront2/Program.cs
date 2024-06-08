c7ec07baff53aea760c070232481ffd3e6f67942
AnaClaraAlmeida <anaclaraalmeida.jf@gmail.com>
2024-06-08 16:50:58 -0300

    x

--- reclameiFront2/Program.cs
+++ reclameiFront2/Program.cs
@@ -1,12 +1,11 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using reclameiFront;
+using reclameiFront2;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-//builder.Services.AddScoped(sp => new Api("http://localhost:5024"));
 
 await builder.Build().RunAsync();
